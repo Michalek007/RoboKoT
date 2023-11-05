208f605f29b635f70849bdd66f96e0ffe89a9b94
Michalek007 <nmichal02@op.pl>
2023-11-05 01:39:41 +0100

    Added: connectToWiFi function in esp->main.cpp

--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -1,13 +1,33 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
 
 // put function declarations here:
+void connectToWiFi(const char*, const char*);
+
+const char* ssid     = "DESKTOP-O2OLJ1H 8056";  // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "L5w7393^";  // The password of the Wi-Fi network
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  connectToWiFi(ssid, password);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+// put function definitions here:
+void connectToWiFi(const char* ssid, const char* password) {
+  WiFi.begin(ssid, password);
+   Serial.println("Connecting");
+   while(WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print(".");
+   }
+   Serial.println("");
+   Serial.print("Connected to WiFi network with IP Address: ");
+   Serial.println(WiFi.localIP());
 }
