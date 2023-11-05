f6393752b94af1f675e60ec400b3f3f588c64859
Michalek007 <nmichal02@op.pl>
2023-11-05 12:58:04 +0100

    Added: requests to api in esp/main.cpp, README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# RoboKoT
+Software for RoboKoT project.
+
+## Project structure
+
+### API
+Rest-api written in Python as Flask app.
+
+### ESP
+
+Embedded code for microcontroller ESP32 (C++).
+
+Functionalities:
+- connects to WiFi
+- makes requests to api
+
+## Installation
+
+First, clone this repository.
+
+    $ git clone https://github.com/Michalek007/RoboKoT.git
+
+Then read `README.md` of each component, which contains information
+how to develop and deploy the project.
--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
 
 
 // put function declarations here:
@@ -7,6 +8,10 @@ void connectToWiFi(const char*, const char*);
 
 const char* ssid     = "DESKTOP-O2OLJ1H 8056";  // The SSID (name) of the Wi-Fi network you want to connect to
 const char* password = "L5w7393^";  // The password of the Wi-Fi network
+WiFiClient client;
+HTTPClient http;
+String servername = "http://192.168.1.15:5000";
+String urlGetAction = servername + "/get_action/";
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,6 +21,15 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  http.begin(client, urlGetAction.c_str());
+  int httpResponseCode = http.GET();
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String payload = http.getString();
+    Serial.println(payload);
+  }
 
 }
 
